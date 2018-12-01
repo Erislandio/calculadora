f28831679ace6d41bc2da7a3a72a56acd56805f5
Erislandio Soares <erislandiosoares21@gmail.com>
2018-12-01 00:30:03 -0200

    Ajustes ds funcao dos botoes

--- src/main/Calculadora.jsx
+++ src/main/Calculadora.jsx
@@ -6,6 +6,16 @@ import Display from '../components/display/Display';
 class Calculadora extends Component {
 
 
+    constructor(props) {
+        super(props);
+
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOpetation = this.setOpetation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+
+    }
+
+
     clearMemory = () => {
         console.log('limpar')
     }
@@ -20,30 +30,30 @@ class Calculadora extends Component {
 
     render() {
 
-        const addDigit = n => this.addDigit(n) 
+        const addDigit = n => this.addDigit(n)
         const setOpetation = op => this.setOpetation(op)
 
 
         return (
             <div className="calculator">
-                <Display value="100"/>
-                <Button label="AC"/>
-                <Button label="/"/>
-                <Button label="7"/>
-                <Button label="8"/>
-                <Button label="9"/>
-                <Button label="*"/>
-                <Button label="4"/>
-                <Button label="5"/>
-                <Button label="6"/>
-                <Button label="-"/>
-                <Button label="1"/>
-                <Button label="2"/>
-                <Button label="3"/>
-                <Button label="+"/>
-                <Button label="0"/>
-                <Button label="."/>
-                <Button label="="/>
+                <Display value="100" />
+                <Button label="AC" click={() => this.clearMemory()} />
+                <Button label="/" click={this.setOpetation} />
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" click={this.setOpetation} />
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" click={this.setOpetation} />
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" click={this.setOpetation} />
+                <Button label="0" click={this.addDigit} />
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOpetation} />
             </div>
         );
     }
