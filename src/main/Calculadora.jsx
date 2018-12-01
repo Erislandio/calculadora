27a3f788cd49e61ccc8c0b250651b5c38425eb54
Erislandio Soares <erislandiosoares21@gmail.com>
2018-11-30 23:29:17 -0200

    first commit

---
+++ src/main/Calculadora.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calculadora extends Component {
+    render() {
+        return (
+            <div>
+                cal
+            </div>
+        );
+    }
+}
+
+export default Calculadora;
